16a49f4d5f81fce7e60dbec655ef6b6f4411b664
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-07-14 21:05:19 +0530

    Add Multiple Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { DotLoader } from "react-spinners";
+import Header from "./components/Header/Header";
 
 function App() {
-  return (
-    <>
-      <div>
-        <h1>Blog Web App With Appwrite</h1>
+  const [loading, setLoading] = useState(true);
+  // Used to combine react and redux
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout({ userData }));
+        }
+      })
+      .catch()
+      .finally(() => setLoading(false));
+
+    return () => {};
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-slate-400">
+      <div className="w-full block">
+        <Header />
+        <main>{/* <Outlet /> */}</main>
       </div>
-    </>
+    </div>
+  ) : (
+    <div>
+      <DotLoader />
+    </div>
   );
 }
 
---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/auth";
+import { logout } from "../../store/authSlice";
+import { toast } from "react-toastify";
+
+const LogoutBtn = () => {
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    authService.logout().then(() => {
+      dispatch(logout()), toast.success("Logout Succesfully");
+    });
+  };
+  return (
+    <button className="inline-block px-6 py-2 duration-200 hover:bg-red-100 rounded-full">
+      Logout
+    </button>
+  );
+};
+
+export default LogoutBtn;
